0ad92f9a37d7f8e9a708322a2b8213cff60353a0
WullusTimon <timon.wullus@student.howest.be>
2022-01-27 15:18:37 +0100

    report yes button

--- smartLocker/js/reportPopup.js
+++ smartLocker/js/reportPopup.js
@@ -5,6 +5,13 @@
   const closeReportButton = document.getElementById('closeReportPopup')
   const popupReport = document.getElementById('popupReport')
   const overlay = document.getElementById('overlay')
+  const yesReport = document.getElementById('yesReport')
+  // const disabled = document.getElementById('lockerDisabled')
+  // const enabled = document.getElementById('lockerEnabled')
+
+  yesReport.addEventListener('click', () => {
+    pushToDisable();
+  })
 
   openReportButton.addEventListener('click', (e) => {
     e.preventDefault();
@@ -25,5 +32,12 @@
     console.log("close report popup")
     popupReport.classList.remove('active')
     overlay.classList.remove('active')
+  }
+
+  const pushToDisable = () => {
+    console.log('disabled')
+    window.location.href = "../pages/lockers.html";
+    // enabled.classList.add('hidden')
+    // disabled.classList.remove('hidden')
   }
 }
